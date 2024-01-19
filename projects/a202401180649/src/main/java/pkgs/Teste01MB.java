7456d136b7713c8489688ec1ba3f1768cff146d9
marciomaiabr <marcio.luis.maia@gmail.com>
2024-01-19 08:32:13 -0400

    PanelGrid, InputText, OutputLabel e CommandButton

--- projects/a202401180649/src/main/java/pkgs/Teste01MB.java
+++ projects/a202401180649/src/main/java/pkgs/Teste01MB.java
@@ -20,6 +20,30 @@ public class Teste01MB {
 		return "20240118_0809";
 	}
 
+	private String nome;
+	private String email;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void cadastrar() {
+		System.out.println("Teste01MB.cadastrar()");
+		System.out.println("[this.nome="+this.nome+"][this.email="+this.email+"]");
+	}
+
 	@PreDestroy
 	public void preDestroy() {
 		System.out.println("Teste01MB.preDestroy()");
