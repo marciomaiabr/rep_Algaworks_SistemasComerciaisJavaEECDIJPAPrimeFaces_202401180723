b3750d7b5afedcd8bd97f59b4b436e9bbcceade1
marciomaiabr <marcio.luis.maia@gmail.com>
2024-02-05 10:12:27 -0400

    reset 202402051012

--- projects/a202401180649/src/main/java/pkgs/Teste01MB.java
+++ projects/a202401180649/src/main/java/pkgs/Teste01MB.java
@@ -1,15 +1,9 @@
 package pkgs;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
 
 @ManagedBean
 @ViewScoped
@@ -25,33 +19,11 @@ public class Teste01MB {
 	}
 
 	public String getMBVersion() {
-		return "20240118_0809";
-	}
-
-	private String nome;
-	private Boolean aceito;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public Boolean getAceito() {
-		return aceito;
-	}
-
-	public void setAceito(Boolean aceito) {
-		this.aceito = aceito;
+		return "20240205_1002";
 	}
 
 	public void atualizar() {
 		System.out.println("Teste01MB.atualizar()");
-		System.out.println("[this.nome=" + this.nome + "][this.aceito=" + this.aceito + "]");
-
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Perfil atualizado !"));
 	}
 
 	@PreDestroy
