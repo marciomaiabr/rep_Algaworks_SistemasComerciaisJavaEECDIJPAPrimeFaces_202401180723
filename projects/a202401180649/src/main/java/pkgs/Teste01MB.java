7e2fa384c2113df9d1a881d94c5055e2114f190a
marciomaiabr <marcio.luis.maia@gmail.com>
2024-02-08 09:47:07 -0400

    reset 202402080946

--- projects/a202401180649/src/main/java/pkgs/Teste01MB.java
+++ projects/a202401180649/src/main/java/pkgs/Teste01MB.java
@@ -1,29 +1,17 @@
 package pkgs;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
 
 @ManagedBean
 @ViewScoped
 public class Teste01MB implements Serializable {
 
 	private static final long serialVersionUID = -7424888261504715069L;
-
-	private List<String> produtos = new ArrayList<>();
-	private String nomeProduto;
-	private String produtoSelecionado;
 
 	public Teste01MB() {
 		System.out.println("Teste01MB.Teste01MB()");
@@ -32,45 +20,10 @@ public class Teste01MB implements Serializable {
 	@PostConstruct
 	public void postConstruct() {
 		System.out.println("Teste01MB.postConstruct()");
-		produtos.add("Arroz");
-		produtos.add("Feijão");
-		produtos.add("Queijo");
-		produtos.add("Notbook");
 	}
 
 	public String getMBVersion() {
 		return "20240118_0809";
-	}
-
-	public List<String> getProdutos() {
-		return produtos;
-	}
-
-	public String getNomeProduto() {
-		return nomeProduto;
-	}
-
-	public void setNomeProduto(String nomeProduto) {
-		this.nomeProduto = nomeProduto;
-	}
-
-	public String getProdutoSelecionado() {
-		return produtoSelecionado;
-	}
-
-	public void setProdutoSelecionado(String produtoSelecionado) {
-		this.produtoSelecionado = produtoSelecionado;
-	}
-
-	public void incluirProduto() {
-		System.out.println("Teste01MB.incluirProduto()");
-		produtos.add(nomeProduto);
-		nomeProduto = null;
-	}
-
-	public void excluirProduto() {
-		System.out.println("Teste01MB.excluirProduto()");
-		produtos.remove(produtoSelecionado);
 	}
 
 	@PreDestroy
