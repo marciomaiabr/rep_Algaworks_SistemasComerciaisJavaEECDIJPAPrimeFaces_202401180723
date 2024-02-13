d49d151ea18d2e5842379dda51666076e7248002
marciomaiabr <marcio.luis.maia@gmail.com>
2024-02-13 08:29:25 -0400

    Criando um produtor CDI para injetar EntityManager

--- projects/a202401180649/src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
+++ projects/a202401180649/src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
@@ -1,10 +1,15 @@
 package com.algaworks.pedidovenda.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
-import javax.faces.bean.ViewScoped;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import com.algaworks.pedidovenda.model.Categoria;
 import com.algaworks.pedidovenda.model.Produto;
 
 @Named
@@ -15,8 +20,20 @@ public class CadastroProdutoBean implements Serializable {
 
 	private Produto produto;
 	
+	private List<Categoria> categoriasRaizes;
+	
 	public CadastroProdutoBean() {
 		produto = new Produto();
+	}
+
+	public void inicializar() {
+		System.out.println("CadastroProdutoBean.inicializar()");
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("PedidoPU");
+		EntityManager manager = factory.createEntityManager();
+		
+		categoriasRaizes = manager.createQuery("from Categoria", Categoria.class).getResultList();
+		
+		manager.close();
 	}
 	
 	public void salvar() {
@@ -24,6 +41,10 @@ public class CadastroProdutoBean implements Serializable {
 
 	public Produto getProduto() {
 		return produto;
+	}
+
+	public List<Categoria> getCategoriasRaizes() {
+		return categoriasRaizes;
 	}
 
 }
