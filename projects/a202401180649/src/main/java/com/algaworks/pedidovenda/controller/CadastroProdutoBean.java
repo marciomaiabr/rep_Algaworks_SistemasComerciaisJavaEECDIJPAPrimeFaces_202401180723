f6a0d02e59670e54aba2af73ce6d988591431c9a
marciomaiabr <marcio.luis.maia@gmail.com>
2024-02-13 04:58:38 -0400

    Adicionando restrições no cadastro de produto

--- projects/a202401180649/src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
+++ projects/a202401180649/src/main/java/com/algaworks/pedidovenda/controller/CadastroProdutoBean.java
@@ -2,8 +2,7 @@ package com.algaworks.pedidovenda.controller;
 
 import java.io.Serializable;
 
-import javax.enterprise.context.RequestScoped;
-import javax.faces.view.ViewScoped;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 
 import com.algaworks.pedidovenda.model.Produto;
@@ -21,11 +20,10 @@ public class CadastroProdutoBean implements Serializable {
 	}
 	
 	public void salvar() {
-		throw new RuntimeException("Teste de exceção.");
 	}
-	
+
 	public Produto getProduto() {
 		return produto;
 	}
-	
+
 }
--- projects/a202401180649/src/main/java/com/algaworks/pedidovenda/model/Produto.java
+++ projects/a202401180649/src/main/java/com/algaworks/pedidovenda/model/Produto.java
@@ -10,7 +10,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 @Table(name="produto")
@@ -35,6 +40,8 @@ public class Produto implements Serializable {
 		this.id = id;
 	}
 
+	@NotBlank
+	@Size(max = 80)
 	@Column(nullable = false, length = 80)
 	public String getNome() {
 		return nome;
@@ -44,6 +51,7 @@ public class Produto implements Serializable {
 		this.nome = nome;
 	}
 
+	@NotBlank
 	@Column(nullable = false, length = 20, unique = true)
 	public String getSku() {
 		return sku;
@@ -53,6 +61,7 @@ public class Produto implements Serializable {
 		this.sku = sku == null ? null : sku.toUpperCase();
 	}
 
+	@NotNull
 	@Column(name="valor_unitario", nullable = false, precision = 10, scale = 2)
 	public BigDecimal getValorUnitario() {
 		return valorUnitario;
@@ -62,8 +71,8 @@ public class Produto implements Serializable {
 		this.valorUnitario = valorUnitario;
 	}
 
+	@NotNull @Min(0) @Max(9999)
 	@Column(name="quantidade_estoque", nullable = false, length = 5)
-	@NotNull
 	public Integer getQuantidadeEstoque() {
 		return quantidadeEstoque;
 	}
@@ -72,6 +81,7 @@ public class Produto implements Serializable {
 		this.quantidadeEstoque = quantidadeEstoque;
 	}
 
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "categoria_id", nullable = false)
 	public Categoria getCategoria() {
